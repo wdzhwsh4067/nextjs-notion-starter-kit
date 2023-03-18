b7dd012464bb60f02ab51e4202acc7bb036c953b
Amber <77528985+wdzhwsh4067@users.noreply.github.com>
2023-03-18 14:08:29 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,7 +9,7 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: 'Dana Wang Blog',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
   author: 'Travis Fischer',
 
